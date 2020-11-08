5ea8d803e748d6ff0a8559c5c12222284cfe7312
Daniel <31024419+yang05051@users.noreply.github.com>
2020-11-08 13:03:34 +0800

    Update WordPress_V2Ray.sh

--- WordPress_V2Ray.sh
+++ WordPress_V2Ray.sh
@@ -2,66 +2,77 @@
 # v2ray centos7/8 WordPress一键安装脚本
 # Author: hijk<https://hijk.art>
 
-echo "#############################################################"
-echo "#             CentOS 7/8 WordPress一键安装脚本                #"
-echo "# 网址: https://hijk.art                                  #"
-echo "# 作者: hijk                                                #"
-echo "#############################################################"
-echo ""
+RED="\033[31m"      # Error message
+GREEN="\033[32m"    # Success message
+YELLOW="\033[33m"   # Warning message
+BLUE="\033[36m"     # Info message
+PLAIN='\033[0m'
 
-red='\033[0;31m'
-green="\033[0;32m"
-plain='\033[0m'
+colorEcho() {
+    echo -e "${1}${@:2}${PLAIN}"
+}
 
-function checkSystem()
-{
+V2_CONFIG_FILE="/etc/v2ray/config.json"
+
+checkSystem() {
     result=$(id | awk '{print $1}')
     if [ $result != "uid=0(root)" ]; then
-        echo "请以root身份执行该脚本"
+        colorEcho $RED " 请以root身份执行该脚本"
         exit 1
     fi
 
     if [ ! -f /etc/centos-release ];then
         res=`which yum`
         if [ "$?" != "0" ]; then
-            echo "系统不是CentOS"
+            colorEcho $RED " 系统不是CentOS"
             exit 1
          fi
     else
         result=`cat /etc/centos-release|grep -oE "[0-9.]+"`
-        main=${result%%.*}
-        if [ $main -lt 7 ]; then
-            echo "不受支持的CentOS版本"
+        MAIN=${result%%.*}
+        if [ $MAIN -lt 7 ]; then
+            colorEcho $RED " 不受支持的CentOS版本"
             exit 1
          fi
     fi
 }
 
-function checkV2()
-{
-    echo "该脚本仅适用于 https://hijk.art 网站的v2ray带伪装一键脚本 安装wordpress用！"
-    read -p "退出请按n，按其他键继续：" answer
+slogon() {
+    clear
+    echo "#############################################################"
+    echo -e "#             ${RED}CentOS 7/8 WordPress一键安装脚本${PLAIN}                #"
+    echo -e "# ${GREEN}作者${PLAIN}: 网络跳越(hijk)                                      #"
+    echo -e "# ${GREEN}网址${PLAIN}: https://hijk.art                                    #"
+    echo -e "# ${GREEN}论坛${PLAIN}: https://hijk.club                                   #"
+    echo -e "# ${GREEN}TG群${PLAIN}: https://t.me/hijkclub                               #"
+    echo -e "# ${GREEN}Youtube频道${PLAIN}: https://youtube.com/channel/UCYTB--VsObzepVJtc9yvUxQ #"
+    echo "#############################################################"
+    echo ""
+}
+
+checkV2() {
+    colorEcho $YELLOW " 该脚本仅适用于 https://hijk.art 网站的v2ray带伪装一键脚本 安装wordpress用！"
+    read -p " 退出请按n，按其他键继续：" answer
     [ "$answer" = "n" ] && exit 0
 
-    if [ ! -f /etc/v2ray/config.json ]; then
-        echo "未安装v2ray"
+    if [ ! -f $V2_CONFIG_FILE ]; then
+        colorEcho $RED " 未安装v2ray"
         exit 1
     fi
-    domain=`cat /etc/v2ray/config.json | grep Host | cut -d: -f2 | tr -d \",' '`
-    if [ "$domain" = "" ]; then
-        echo "未检测到配置了ws协议的v2ray"
+    DOMAIN=`grep Host $V2_CONFIG_FILE | cut -d: -f2 | tr -d \",' '`
+    if [ "$DOMAIN" = "" ]; then
+        colorEcho $RED " 未检测到配置了ws协议的v2ray"
         exit 1
     fi
-    if [ ! -f /etc/nginx/conf.d/${domain}.conf ]; then
-        echo "未找到域名的nginx配置文件"
+    if [ ! -f /etc/nginx/conf.d/${DOMAIN}.conf ]; then
+        colorEcho $RED " 未找到域名的nginx配置文件"
         exit 1
     fi
 }
 
-function installPHP()
-{
+installPHP() {
     yum install -y epel-release
-    if [ $main -eq 7 ]; then
+    if [ $MAIN -eq 7 ]; then
         rpm -iUh https://rpms.remirepo.net/enterprise/remi-release-7.rpm
 	    sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/remi-php74.repo
     else
@@ -73,70 +84,67 @@ function installPHP()
     systemctl enable php-fpm.service
 }
 
-function installMysql()
-{
+installMysql() {
     yum remove -y MariaDB-server
     rm -rf /var/lib/mysql
     if [ ! -f /etc/yum.repos.d/mariadb.repo ]; then
-    	if [ $main -eq 7 ]; then
-    	echo '# MariaDB 10.4 CentOS repository list - created 2019-11-23 15:00 UTC
+    	if [ $MAIN -eq 7 ]; then
+    	    echo '# MariaDB 10.5 CentOS repository list - created 2019-11-23 15:00 UTC
 # http://downloads.mariadb.org/mariadb/repositories/
 [mariadb]
 name = MariaDB
-baseurl = http://yum.mariadb.org/10.4/centos7-amd64
+baseurl = http://yum.mariadb.org/10.5/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1' >> /etc/yum.repos.d/mariadb.repo
-	else
-echo '# MariaDB 10.4 CentOS repository list - created 2020-03-11 16:29 UTC
+	    else
+            echo '# MariaDB 10.5 CentOS repository list - created 2020-03-11 16:29 UTC
 # http://downloads.mariadb.org/mariadb/repositories/
 [mariadb]
 name = MariaDB
-baseurl = http://yum.mariadb.org/10.4/centos8-amd64
+baseurl = http://yum.mariadb.org/10.5/centos8-amd64
 module_hotfixes=1
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1' >>  /etc/yum.repos.d/mariadb.repo
-	fi
+	    fi
     fi
     yum install -y MariaDB-server
     systemctl enable mariadb.service
 }
 
-function installWordPress()
-{
+installWordPress() {
     yum install -y wget
     mkdir -p /var/www;
     wget https://www.wordpress.org/latest.tar.gz
     if [ ! -f latest.tar.gz ]; then
-    	echo "下载WordPress失败，请稍后重试"
-	exit 1
+    	colorEcho $RED " 下载WordPress失败，请稍后重试"
+	    exit 1
     fi
     tar -zxf latest.tar.gz
-    rm -rf /var/www/$domain
-    mv wordpress /var/www/$domain
+    rm -rf /var/www/$DOMAIN
+    mv wordpress /var/www/$DOMAIN
     rm -rf latest.tar.gz
 }
 
-function config()
-{
+config() {
     # config mariadb
     systemctl start mariadb
-    dbname="wordpress"
-    dbuser="wordpress"
-    dbpass=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1`
+    DBNAME="wordpress"
+    DBUSER="wordpress"
+    DBPASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1`
     mysql -uroot <<EOF
 DELETE FROM mysql.user WHERE User='';
-CREATE DATABASE $dbname default charset utf8mb4;
-CREATE USER ${dbuser}@'%' IDENTIFIED BY '${dbpass}';
-GRANT ALL PRIVILEGES ON ${dbname}.* to ${dbuser}@'%';
+CREATE DATABASE $DBNAME default charset utf8mb4;
+CREATE USER ${DBUSER}@'%' IDENTIFIED BY '${DBPASS}';
+GRANT ALL PRIVILEGES ON ${DBNAME}.* to ${DBUSER}@'%';
 FLUSH PRIVILEGES;
 EOF
 
     # config wordpress
-    cd /var/www/$domain
+    cd /var/www/$DOMAIN
     cp wp-config-sample.php wp-config.php
-    sed -i "s/database_name_here/$dbname/g" wp-config.php
-    sed -i "s/username_here/$dbuser/g" wp-config.php
-    sed -i "s/password_here/$dbpass/g" wp-config.php
+    sed -i "s/database_name_here/$DBNAME/g" wp-config.php
+    sed -i "s/username_here/$DBUSER/g" wp-config.php
+    sed -i "s/password_here/$DBPASS/g" wp-config.php
     sed -i "s/utf8/utf8mb4/g" wp-config.php
     perl -i -pe'
   BEGIN {
@@ -146,17 +154,17 @@ EOF
   }
   s/put your unique phrase here/salt()/ge
 ' wp-config.php
-    chown -R apache:apache /var/www/${domain}
+    chown -R apache:apache /var/www/${DOMAIN}
 
     # config nginx
-    sed -i '26,29d' /etc/nginx/conf.d/${domain}.conf
-    sed -i '$d' /etc/nginx/conf.d/${domain}.conf
-    if [ $main -eq 7 ]; then
+    sed -i '26,29d' /etc/nginx/conf.d/${DOMAIN}.conf
+    sed -i '$d' /etc/nginx/conf.d/${DOMAIN}.conf
+    if [ $MAIN -eq 7 ]; then
         upstream="127.0.0.1:9000"
     else
         upstream="php-fpm"
     fi
-    echo "  set \$host_path "/var/www/${domain}";
+    echo "  set \$host_path "/var/www/${DOMAIN}";
     root   \$host_path;
     location / {
         index  index.php index.html;
@@ -172,26 +180,25 @@ EOF
         include fastcgi_params;
         fastcgi_param  SCRIPT_FILENAME  \$document_root\$fastcgi_script_name;
     }
-}" >> /etc/nginx/conf.d/${domain}.conf
+}" >> /etc/nginx/conf.d/${DOMAIN}.conf
 
     # restart service
     systemctl restart php-fpm mariadb nginx
 }
 
-function info()
-{
-    echo "WordPress安装成功！"
+info() {
+    colorEcho $BLUE " WordPress安装成功！"
     echo "==============================="
-    echo -e "WordPress安装路径：${red}/var/www/${domain}${plain}"
-    echo -e "WordPress数据库：${red}${dbname}${plain}"
-    echo -e "WordPress数据库用户名：${red}${dbuser}${plain}"
-    echo -e "WordPress数据库密码：${red}${dbpass}${plain}"
-    echo -e "博客访问地址：${red}https://${domain}${plain}"
+    echo -e "   ${BLUE}WordPress安装路径：${PLAIN}${RED}/var/www/${DOMAIN}${PLAIN}"
+    echo -e "   ${BLUE}WordPress数据库：${PLAIN}${RED}${DBNAME}${PLAIN}"
+    echo -e "   ${BLUE}WordPress数据库用户名：${PLAIN}${RED}${DBUSER}${PLAIN}"
+    echo -e "   ${BLUE}WordPress数据库密码：${PLAIN}${RED}${DBPASS}${PLAIN}"
+    echo -e "   ${BLUE}博客访问地址：${PLAIN}${RED}https://${DOMAIN}${PLAIN}"
     echo "==============================="
 }
 
-function main()
-{
+main() {
+    slogon
     checkSystem
     checkV2
     installPHP
