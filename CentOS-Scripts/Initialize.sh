879476ba85e87a43cdf1cff00539e896306a0924
Daniel <yang05051@outlook.com>
2021-07-24 22:20:27 +0800

    Update CentOS-Scripts (20210724-1)

--- CentOS-Scripts/Initialize.sh
+++ CentOS-Scripts/Initialize.sh
@@ -1,3 +1,4 @@
 bash <(curl -sL https://raw.githubusercontent.com/yang05051/Daniel/main/CentOS-Scripts/CentOS-to-Stream.sh)
 bash <(curl -sL https://raw.githubusercontent.com/yang05051/Daniel/main/CentOS-Scripts/Install-Nginx-Mainline.sh)
 bash <(curl -sL https://raw.githubusercontent.com/yang05051/Daniel/main/CentOS-Scripts/Install-Latest-Kernel.sh)
+reboot
