784a174cc75093261e613c99c510f486b69c56fc
Daniel <yang05051@outlook.com>
2021-09-12 10:39:55 +0800

    Update Install-Nginx-Mainline.sh

--- RHEL-Scripts/Install-Nginx-Mainline.sh
+++ RHEL-Scripts/Install-Nginx-Mainline.sh
@@ -1,6 +1,6 @@
 echo '[nginx-mainline]
 name=nginx mainline repo
-baseurl=http://nginx.org/packages/mainline/rhel/$releasever/$basearch/
+baseurl=http://nginx.org/packages/mainline/rhel/8/$basearch/
 gpgcheck=1
 enabled=0
 gpgkey=https://nginx.org/keys/nginx_signing.key
