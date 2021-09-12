b4ed1ce9ea4059498bf113c1c65ef9f59c785a1e
Daniel <yang05051@outlook.com>
2021-09-12 13:19:23 +0800

    Update Install-Latest-Kernel.sh

--- RHEL-Scripts/Install-Latest-Kernel.sh
+++ RHEL-Scripts/Install-Latest-Kernel.sh
@@ -1,5 +1,6 @@
 dnf install -y https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 dnf makecache
+sed -i 's/$releasever/8/g' /etc/yum.repos.d/epel*.repo
 yum-config-manager --enable elrepo-kernel
 dnf install -y kernel-ml
