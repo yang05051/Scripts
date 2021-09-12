67030ba606ee30671178ce910104cc61c67eabe3
Daniel <yang05051@outlook.com>
2021-09-12 10:37:25 +0800

    Add RHEL Scripts

---
+++ RHEL-Scripts/Install-Latest-Kernel.sh
@@ -0,0 +1,5 @@
+dnf install -y https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+dnf makecache
+yum-config-manager --enable elrepo-kernel
+dnf install -y kernel-ml
---
+++ RHEL-Scripts/Install-Nginx-Mainline.sh
@@ -0,0 +1,9 @@
+echo '[nginx-mainline]
+name=nginx mainline repo
+baseurl=http://nginx.org/packages/mainline/rhel/$releasever/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true' > /etc/yum.repos.d/nginx.repo
+yum-config-manager --enable nginx-mainline
+yum install nginx -y
