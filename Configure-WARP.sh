497b0ad757b5486dc8895b6fd7c303a3c3eab5ff
Daniel <yang05051@outlook.com>
2021-09-12 13:36:54 +0800

    Update Configure-WARP.sh

--- Configure-WARP.sh
+++ Configure-WARP.sh
@@ -3,7 +3,8 @@ read -p "Enter your network adapter's IP: " ANSIP
 read -p "Enter Public Key: " ANSKEY
 modprobe wireguard
 lsmod | grep wireguard
-echo '[Interface]
+cat > /etc/wireguard/wgcf.conf<<-EOF
+[Interface]
 PrivateKey = qHFuEoF0KW2H66U5lZ4b2cwcnGfWPPyHMq7iCKQ9UnM=
 Address = 172.16.0.2/32
 Address = fd01:5ca1:ab1e:86e2:7865:61f7:b8f8:765/128
@@ -15,6 +16,7 @@ PostDown = ip rule delete from $ANSIP lookup main
 PublicKey = $ANSKEY
 AllowedIPs = 0.0.0.0/0
 AllowedIPs = ::/0
-Endpoint = 162.159.192.1:2408' > /etc/wireguard/wgcf.conf
+Endpoint = 162.159.192.1:2408 
+EOF
 systemctl enable wg-quick@wgcf.service
 systemctl start wg-quick@wgcf
