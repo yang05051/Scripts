b415875d9845b9c143493d1a14fd3c372633d67e
Daniel <yang05051@outlook.com>
2021-09-12 12:45:46 +0800

    Update Configure-WARP.sh

--- Configure-WARP.sh
+++ Configure-WARP.sh
@@ -1,5 +1,5 @@
-read -p "Enter your network adapter's IP:" ans-ip
-read -p "Enter Public Key:" ans-pkey
+read -p "Enter your network adapter's IP:" ANSIP
+read -p "Enter Public Key:" ANSKEY
 modprobe wireguard
 lsmod | grep wireguard
 echo '[Interface]
@@ -8,10 +8,10 @@ Address = 172.16.0.2/32
 Address = fd01:5ca1:ab1e:86e2:7865:61f7:b8f8:765/128
 DNS = 1.1.1.1
 MTU = 1280
-PostUp = ip rule add from ${ans-ip} lookup main
-PostDown = ip rule delete from ${ans-ip} lookup main
+PostUp = ip rule add from ${ANSIP} lookup main
+PostDown = ip rule delete from ${ANSIP} lookup main
 [Peer]
-PublicKey = ${ans-pkey}
+PublicKey = ${ANSKEY}
 AllowedIPs = 0.0.0.0/0
 AllowedIPs = ::/0
 Endpoint = 162.159.192.1:2408' > /etc/wireguard/wgcf.conf
