d3e104c2a40327cb90896df4bebf1e3b2c3818db
Daniel <31024419+yang05051@users.noreply.github.com>
2022-02-02 14:50:30 +0000

    Update Configure-WARP.sh

--- Configure-WARP.sh
+++ Configure-WARP.sh
@@ -1,12 +1,13 @@
 yum install epel-release -y
 yum install wireguard-tools -y
-read -p "Enter your network adapter's IP: " ANSIP
-read -p "Enter your public key of WARP: " ANSKEY
+read -p "Enter your IP: " ANSIP
+read -p "Enter your private key of WARP: " ANSPRVKEY
+read -p "Enter your public key of WARP: " ANSPUBKEY
 modprobe wireguard
 lsmod | grep wireguard
 cat > /etc/wireguard/wgcf.conf<<-EOF
 [Interface]
-PrivateKey = qHFuEoF0KW2H66U5lZ4b2cwcnGfWPPyHMq7iCKQ9UnM=
+PrivateKey = $ANSPRVKEY
 Address = 172.16.0.2/32
 Address = fd01:5ca1:ab1e:86e2:7865:61f7:b8f8:765/128
 DNS = 1.1.1.1
