49e141d24337dc1b89b816727b968b67636b8edb
Daniel <yang05051@outlook.com>
2022-02-09 12:55:55 +0000

    Catagorize

--- CentOS-Scripts/CentOS-Initialize.sh
+++
@@ -1,2 +0,0 @@
-bash <(curl -sL https://raw.githubusercontent.com/yang05051/Daniel/main/CentOS-Scripts/Install-Nginx-Mainline.sh)
-bash <(curl -sL https://raw.githubusercontent.com/yang05051/Daniel/main/CentOS-Scripts/Install-Latest-Kernel.sh)
