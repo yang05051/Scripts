bfc2b78afe9c36a110f76652ffe236b3b010f15f
Daniel <31024419+yang05051@users.noreply.github.com>
2022-02-17 04:42:45 +0000

    Create Acme-Issue-Wildcard-Cert_Verify-Vultr-DNS.sh

---
+++ Acme-Issue-Wildcard-Cert_Verify-Vultr-DNS.sh
@@ -0,0 +1,6 @@
+yum install socat -y
+curl https://get.acme.sh | sh -s email=my@example.com
+read -p "Enter your domain: " ANSDOMAIN
+read -p "Enter your Vultr API key: " ANSAPIKEY
+export VULTR_API_KEY="$ANSAPIKEY"
+/root/.acme.sh/acme.sh --issue --dns dns_vultr -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file /root/xray.key --fullchain-file /root/xray.pem --renew-hook "systemctl restart nginx xray"
