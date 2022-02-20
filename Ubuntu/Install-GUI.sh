19830725775aa2e0ef0db42caf20ab4166b4348a
Daniel <31024419+yang05051@users.noreply.github.com>
2022-02-20 04:16:17 +0000

    Update Install-GUI.sh

--- Ubuntu/Install-GUI.sh
+++ Ubuntu/Install-GUI.sh
@@ -1,6 +1,6 @@
 apt update -y
 apt upgrade -y
-apt install ubuntu-desktop -y
+apt install slim ubuntu-desktop -y
 apt install xrdp xorg dbus-x11 x11-xserver-utils -y
 adduser xrdp ssl-cert
 systemctl restart xrdp
