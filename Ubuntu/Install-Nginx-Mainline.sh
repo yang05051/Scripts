207ee0d4317be36e2e847716a12b8bb36afacee3
Daniel <31024419+yang05051@users.noreply.github.com>
2022-03-27 04:29:15 +0000

    Update Install-Nginx-Mainline.sh

--- Ubuntu/Install-Nginx-Mainline.sh
+++ Ubuntu/Install-Nginx-Mainline.sh
@@ -2,11 +2,11 @@ wget http://nginx.org/keys/nginx_signing.key
 sudo apt-key add nginx_signing.key
 if [[ -r /etc/os-release ]]; then
     . /etc/os-release
+    echo 'deb http://nginx.org/packages/mainline/ubuntu/ $VERSION_CODENAME nginx'  >> /etc/apt/sources.list
+    echo 'deb-src http://nginx.org/packages/mainline/ubuntu/ $VERSION_CODENAME nginx' >> /etc/apt/sources.list
 else
     echo "Not running a distribution with /etc/os-release available"
     exit 1;
 fi
-echo 'deb http://nginx.org/packages/mainline/ubuntu/ $VERSION_CODENAME nginx
-deb-src http://nginx.org/packages/mainline/ubuntu/ $VERSION_CODENAME nginx' >> /etc/apt/sources.list
 apt update -y
 apt install nginx -y
