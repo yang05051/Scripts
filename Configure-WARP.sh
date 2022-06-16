4829f3d345ebff755bfb62f2396c47ff30e69bd3
Daniel <31024419+yang05051@users.noreply.github.com>
2022-06-16 06:00:50 +0000

    Update Configure-WARP.sh

--- Configure-WARP.sh
+++ Configure-WARP.sh
@@ -19,14 +19,14 @@ PrivateKey = $ANSPRVKEY
 Address = 172.16.0.2/32
 Address = fd01:5ca1:ab1e:86e2:7865:61f7:b8f8:765/128
 DNS = 1.1.1.1
-MTU = 1280
+MTU = 1420
 PostUp = ip rule add from $ANSIP lookup main
 PostDown = ip rule delete from $ANSIP lookup main
 [Peer]
 PublicKey = $ANSPUBKEY
 AllowedIPs = 0.0.0.0/0
 AllowedIPs = ::/0
-Endpoint = 162.159.192.1:2408 
+Endpoint = engage.cloudflareclient.com:2408 
 EOF
 systemctl enable wg-quick@wgcf.service
 systemctl start wg-quick@wgcf
