28e3a4cbe9060556e0b95e3a2dc3e0300f62c080
Daniel <31024419+yang05051@users.noreply.github.com>
2022-08-17 10:52:21 +0800

    Update

--- Configure_UFW.sh
+++ Configure_UFW.sh
@@ -1,7 +1,12 @@
-ufw default deny incoming
-ufw default allow outgoing
-ufw allow ssh
-ufw allow http
-ufw allow https
-systemctl enable ufw
-systemctl start ufw
+if [[ ! -z $(which ufw) ]]; then
+    ufw default deny incoming
+    ufw default allow outgoing
+    ufw allow ssh
+    ufw allow http
+    ufw allow https
+    systemctl enable ufw
+    systemctl start ufw
+else
+    echo "UFW is not installed."
+    exit 1
+fi
---
+++ Disable-ICMP_UFW.sh
@@ -0,0 +1,7 @@
+if [[ ! -z $(which ufw) ]]; then
+    sed -i 's/-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT/-A ufw-before-input -p icmp --icmp-type echo-request -j DROP/g' /etc/ufw/before.rules
+    sed -i 's/-A ufw6-before-input -p icmpv6 --icmpv6-type echo-request -j ACCEPT/-A ufw6-before-input -p icmpv6 --icmpv6-type echo-request -j DROP/g' /etc/ufw/before6.rules
+else
+    echo "UFW is not installed."
+    exit 1;
+fi
