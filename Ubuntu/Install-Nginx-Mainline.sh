c0e7751197eefaa74d9175277ffdf3ce11b06866
Daniel <31024419+yang05051@users.noreply.github.com>
2022-09-25 13:00:48 +0800

    Update Install-Nginx-Mainline.sh

--- Ubuntu/Install-Nginx-Mainline.sh
+++ Ubuntu/Install-Nginx-Mainline.sh
@@ -2,7 +2,12 @@ sudo apt install wget gnupg2 ca-certificates lsb-release ubuntu-keyring software
 wget -O- https://nginx.org/keys/nginx_signing.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg
 if [[ -r /etc/os-release ]]; then
     echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx" >> /etc/apt/sources.list.d/nginx-ml.list
-    echo "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" >> /etc/apt/preferences.d/99nginx
+    cat > /etc/apt/preferences.d/99nginx<<-EOF
+    Package: *
+    Pin: origin nginx.org
+    Pin: release o=nginx
+    Pin-Priority: 900
+    EOF
     ufw enable
     ufw allow http
     ufw allow https
