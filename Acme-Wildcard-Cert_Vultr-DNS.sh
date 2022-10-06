03c2349b93f317ff5de3aac8fba94d43d2191b86
Daniel <31024419+yang05051@users.noreply.github.com>
2022-10-06 19:23:09 +0800

    Update Acme-Wildcard-Cert_Vultr-DNS.sh

--- Acme-Wildcard-Cert_Vultr-DNS.sh
+++ Acme-Wildcard-Cert_Vultr-DNS.sh
@@ -10,4 +10,4 @@ curl https://get.acme.sh | sh -s email=my@example.com
 read -p "Enter your domain: " ANSDOMAIN
 read -p "Enter your Vultr API key: " ANSAPIKEY
 export VULTR_API_KEY="$ANSAPIKEY"
-/root/.acme.sh/acme.sh --issue --dns dns_vultr -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file /root/xray.key --fullchain-file /root/xray.pem --renew-hook "systemctl restart nginx xray"
+/root/.acme.sh/acme.sh --issue --dns dns_vultr -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file /root/xray.key --fullchain-file /root/xray.pem --renew-hook "systemctl restart nginx xray hysteria-server"
