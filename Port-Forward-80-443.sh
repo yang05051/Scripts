dbaa7f310f3ad6d988b61074fc40ac6483138974
Daniel <31024419+yang05051@users.noreply.github.com>
2022-10-08 15:13:52 +0800

    Update Port-Forward-80-443.sh

--- Port-Forward-80-443.sh
+++ Port-Forward-80-443.sh
@@ -1,11 +1,14 @@
 read -p "Enter destination IP/domain: " ANSDEST
-read -p "Enter the port that will redirect to destination port 80: " ANSP80
-read -p "Enter the port that will redirect to destination port 443: " ANSP443
 
 if [[ $ANSDEST == "" ]]; then
     echo "No destination IP/domain entered."
     exit 1;
-elif [[ $ANSP80 == "" && $ANSP443 == "" ]]; then
+fi
+
+read -p "Enter the port that will redirect to destination port 80: (Leave it blank if you do not want to) " ANSP80
+read -p "Enter the port that will redirect to destination port 443: (Leave it blank if you do not want to) " ANSP443
+
+if [[ $ANSP80 == "" && $ANSP443 == "" ]]; then
     echo "No port entered."
     exit 1;
 fi
@@ -19,8 +22,14 @@ else
     echo "Unsupported system."
     exit 1;
 fi
-redir :$ANSP80 $ANSDEST:80
-redir :$ANSP443 $ANSDEST:443
-echo "@reboot root redir :$ANSP80 $ANSDEST:80" >> /etc/crontab
-echo "@reboot root redir :$ANSP443 $ANSDEST:443" >> /etc/crontab
+
+if [[ $ANSP80 != "" ]]; then
+    redir :$ANSP80 $ANSDEST:80
+    echo "@reboot root redir :$ANSP80 $ANSDEST:80" >> /etc/crontab
+fi
+if [[ $ANSP443 != "" ]]; then
+    redir :$ANSP443 $ANSDEST:443
+    echo "@reboot root redir :$ANSP443 $ANSDEST:443" >> /etc/crontab
+fi
+
 bash <(curl -H 'Cache-Control: no-cache' -sL https://raw.githubusercontent.com/yang05051/Scripts/main/Enable-BBR.sh)
