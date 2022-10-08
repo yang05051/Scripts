588c1a1868ae429b4cb6f0e1276ab3e85f63e963
Daniel <31024419+yang05051@users.noreply.github.com>
2022-10-08 15:05:56 +0800

    Update Port-Forward-80-443.sh

--- Port-Forward-80-443.sh
+++ Port-Forward-80-443.sh
@@ -1,4 +1,6 @@
-read -p "Enter your destination IP/domain: " ANSDEST
+read -p "Enter destination IP/domain: " ANSDEST
+read -p "Enter the port that will redirect to destination port 80: " ANSP80
+read -p "Enter the port that will redirect to destination port 443: " ANSP443
 if [[ ! -z $(which yum) ]]; then
     yum install epel-release epel-next-release -y
     yum install redir -y
@@ -8,8 +10,8 @@ else
     echo "Unsupported system."
     exit 1;
 fi
-redir :80 $ANSDEST:80
-redir :443 $ANSDEST:443
-echo "@reboot root redir :80 $ANSDEST:80" >> /etc/crontab
-echo "@reboot root redir :443 $ANSDEST:443" >> /etc/crontab
+redir :$ANSP80 $ANSDEST:80
+redir :$ANSP443 $ANSDEST:443
+echo "@reboot root redir :$ANSP80 $ANSDEST:80" >> /etc/crontab
+echo "@reboot root redir :$ANSP443 $ANSDEST:443" >> /etc/crontab
 bash <(curl -H 'Cache-Control: no-cache' -sL https://raw.githubusercontent.com/yang05051/Scripts/main/Enable-BBR.sh)
