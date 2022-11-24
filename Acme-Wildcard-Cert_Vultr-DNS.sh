d74abb8ff7c79466423380ba75f728cd02849678
Daniel <31024419+yang05051@users.noreply.github.com>
2022-11-24 11:01:46 +0800

    Update Acme-Wildcard-Cert_Vultr-DNS.sh

--- Acme-Wildcard-Cert_Vultr-DNS.sh
+++ Acme-Wildcard-Cert_Vultr-DNS.sh
@@ -1,14 +1,3 @@
-if [[ ! -z $(which yum) ]]; then
-    yum install socat -y
-elif [[ ! -z $(which apt) ]]; then
-    apt install socat -y
-else
-    echo "Unsupported system."
-    exit 1;
-fi
-
-curl https://get.acme.sh | sh -s email=my@example.com
-
 read -p "Enter your domain: " ANSDOMAIN
 read -p "Enter your Vultr API key: " ANSAPIKEY
 read -p "Enter renew hook: (Leave blank to skip) " ANSRENEWHOOK
@@ -24,6 +13,17 @@ if [[ $ANSRENEWHOOK != "" ]]; then
 else
     INFORENEWHOOK=""
 fi
+
+if [[ ! -z $(which yum) ]]; then
+    yum install socat -y
+elif [[ ! -z $(which apt) ]]; then
+    apt install socat -y
+else
+    echo "Unsupported system."
+    exit 1;
+fi
+
+curl https://get.acme.sh | sh -s email=my@example.com
 
 export VULTR_API_KEY="$ANSAPIKEY"
 /root/.acme.sh/acme.sh --issue --dns dns_vultr -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file /root/xray.key --fullchain-file /root/xray.pem$INFORENEWHOOK
