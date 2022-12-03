e97db47413b6e41aa1c573fcc2d8680ee29bc2ba
Daniel <31024419+yang05051@users.noreply.github.com>
2022-12-03 14:13:09 +0800

    Update Install-Emby-Server.sh

--- Install-Emby-Server.sh
+++ Install-Emby-Server.sh
@@ -13,6 +13,7 @@ VER=$(curl -s "https://api.github.com/repos/MediaBrowser/Emby.Releases/releases/
 
 wget https://github.com/MediaBrowser/Emby.Releases/releases/download/$VER/emby-server-deb_$VER_amd64.deb
 dpkg -i emby-server-deb_$VER_amd64.deb
+rm emby-server-deb_$VER_amd64.deb
 
 sed -i 's/User=emby/User=root/g' /lib/systemd/system/emby-server.service
 systemctl daemon-reload
