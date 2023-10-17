8467c233487fd185f47e5d12ac29ad96fc885997
Daniel <31024419+yang05051@users.noreply.github.com>
2023-10-18 03:02:33 +0800

    Update Check-Resolv.conf-Symlink-to-Resolvconf.sh

--- Check-Resolv.conf-Symlink-to-Resolvconf.sh
+++ Check-Resolv.conf-Symlink-to-Resolvconf.sh
@@ -5,7 +5,7 @@ if [[ $ANSSYMLINK != '/run/resolvconf/resolv.conf' ]]; then
     if [[ $ANSSYMLINK == '' ]]; then
         cp /etc/resolv.conf /etc/resolv.conf.bak
     else
-        ln -s ANSSYMLINK /etc/resolv.conf.bak
+        ln -s $ANSSYMLINK /etc/resolv.conf.bak
     fi
     rm /etc/resolv.conf; ln -s /run/resolvconf/resolv.conf /etc/resolv.conf
 fi
