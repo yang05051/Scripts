f61ecd6bf7a6bdfe3314d1ba2ed07dce5160c666
Daniel <31024419+yang05051@users.noreply.github.com>
2023-11-19 21:30:02 +0800

    Update Disable-ICMP_UFW.sh

--- Disable-ICMP_UFW.sh
+++ Disable-ICMP_UFW.sh
@@ -1,4 +1,4 @@
-if [[ ! -z $(which ufw) ]]; then
+if [[ $(which ufw) != '' ]]; then
     sed -i 's/-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT/-A ufw-before-input -p icmp --icmp-type echo-request -j DROP/g' /etc/ufw/before.rules
     sed -i 's/-A ufw6-before-input -p icmpv6 --icmpv6-type echo-request -j ACCEPT/-A ufw6-before-input -p icmpv6 --icmpv6-type echo-request -j DROP/g' /etc/ufw/before6.rules
     ufw reload
