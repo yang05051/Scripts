b10f476c5b3ab3d4c7b1db971d5e17619ce45053
Daniel <31024419+yang05051@users.noreply.github.com>
2023-11-19 21:19:22 +0800

    Update Disable-Password-Auth.sh

--- Disable-Password-Auth.sh
+++ Disable-Password-Auth.sh
@@ -1,11 +1,19 @@
 if [[ $(grep 'Include \/etc\/ssh\/sshd_config.d\/\*\.conf' /etc/ssh/sshd_config) == '' ]]; then
     echo "Include /etc/ssh/sshd_config.d/*.conf" >> /etc/ssh/sshd_config
 else
-    if [[$(grep '^#.*Include \/etc\/ssh\/sshd_config.d\/\*\.conf$' /etc/ssh/sshd_config) == '' ]]; then
+    if [[ $(grep '^#.*Include \/etc\/ssh\/sshd_config.d\/\*\.conf$' /etc/ssh/sshd_config) == '' ]]; then
         sed -i 's/^#.*Include \/etc\/ssh\/sshd_config.d\/\*\.conf$/Include \/etc\/ssh\/sshd_config.d\/\*\.conf/g' /etc/ssh/sshd_config
     fi
 fi
 
 sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config.d/*;
-echo "PasswordAuthentication no" >> /etc/ssh/sshd_config.d/disable-password-auth.conf
+
+if [[ $(ls /etc/ssh/sshd_config.d | grep 'disable-password-auth\.conf') == '' ]]; then
+    echo "PasswordAuthentication no" >> /etc/ssh/sshd_config.d/disable-password-auth.conf
+else
+    if [[ $(grep '^#.*PasswordAuthentication no$' /etc/ssh/sshd_config.d/disable-password-auth.conf) != '' ]]; then
+        sed -i 's/^#.*PasswordAuthentication no$/PasswordAuthentication no/g' /etc/ssh/sshd_config
+    fi
+fi
+
 systemctl restart ssh
