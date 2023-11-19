cf6a5bcd60d47138532c930b69205c0779693584
Daniel <31024419+yang05051@users.noreply.github.com>
2023-11-19 21:04:48 +0800

    Create Disable-Password-Auth.sh

---
+++ Disable-Password-Auth.sh
@@ -0,0 +1,11 @@
+if [[ ! -z $(grep 'Include \/etc\/ssh\/sshd_config.d\/\*\.conf' /etc/ssh/sshd_config) ]]; then
+    echo "Include /etc/ssh/sshd_config.d/*.conf" >> /etc/ssh/sshd_config
+else
+    if [[ ! -z $(grep '^#.*Include \/etc\/ssh\/sshd_config.d\/\*\.conf$' /etc/ssh/sshd_config) ]]; then
+        sed -i 's/^#.*Include \/etc\/ssh\/sshd_config.d\/\*\.conf$/Include \/etc\/ssh\/sshd_config.d\/\*\.conf/g' /etc/ssh/sshd_config
+    fi
+fi
+
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config.d/*;
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config.d/disable-password-auth.conf
+systemctl restart ssh
