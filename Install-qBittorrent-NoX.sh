1d36f3ccae74423e1f3bf6f5ca9b3f75760f9102
Daniel <31024419+yang05051@users.noreply.github.com>
2023-12-01 15:08:48 +0800

    Update Install-qBittorrent-NoX.sh

--- Install-qBittorrent-NoX.sh
+++ Install-qBittorrent-NoX.sh
@@ -4,28 +4,18 @@ elif [[ $(which qbittorrent-nox == '' && $(which yum) != '' ]]
   then yum install qbittorrent-nox -y
 fi
 
-cat > /etc/systemd/system/qbittorrent.service<<-EOF
-[Unit]
+echo '[Unit]
 Description=qBittorrent-nox service
 Documentation=man:qbittorrent-nox(1)
 Wants=network-online.target
 After=network-online.target nss-lookup.target
 
 [Service]
-# if you have systemd < 240 (Ubuntu 18.10 and earlier, for example), you probably want to use Type=simple instead
 Type=exec
-# change user as needed
 User=root
-# The -d flag should not be used in this setup
-ExecStart=/usr/bin/qbittorrent-nox
-# uncomment this for versions of qBittorrent < 4.2.0 to set the maximum number of open files to unlimited
-#LimitNOFILE=infinity
-# uncomment this to use "Network interface" and/or "Optional IP address to bind to" options
-# without this binding will fail and qBittorrent's traffic will go through the default route
-# AmbientCapabilities=CAP_NET_RAW
+ExecStart=/usr/bin/qbittorrent-nox --daemon --webui-port=8097
 
 [Install]
-WantedBy=multi-user.target
-EOF
+WantedBy=multi-user.target' > /etc/systemd/system/qbittorrent.service
 
 systemctl daemon-reload
