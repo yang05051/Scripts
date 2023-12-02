2a15336068b0f1cc7bd9af4fc778ec63e906a604
Daniel <31024419+yang05051@users.noreply.github.com>
2023-12-03 00:12:20 +0800

    Create Install-Stable-qBittorrent-Enhanced-NoX.sh

---
+++ Install-Stable-qBittorrent-Enhanced-NoX.sh
@@ -0,0 +1,26 @@
+read -p 'Enter Web UI port: ' ANSPORT
+
+if [[ $(which qbittorrent-nox) == '' ]]; then
+  if [[ $(which apt) != '' ]]
+    then add-apt-repository ppa:poplite/qbittorrent-enhanced; apt update; apt install qbittorrent-enhanced-nox -y
+  else
+    echo "Unsupported system."
+    exit 1;
+  fi
+fi
+
+echo "[Unit]
+Description=qBittorrent Enhanced Edition Service
+Documentation=man:qbittorrent-nox(1)
+Wants=network-online.target
+After=network-online.target nss-lookup.target
+
+[Service]
+Type=exec
+User=root
+ExecStart=/usr/bin/qbittorrent-nox --webui-port=$ANSPORT
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/qbittorrent.service
+
+systemctl daemon-reload
