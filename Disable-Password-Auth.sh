d24d5ec9fa8974c6eeef04b5cad1a1ccce851a85
Daniel <31024419+yang05051@users.noreply.github.com>
2023-12-03 01:10:59 -0800

    Update Disable-Password-Auth.sh

--- Disable-Password-Auth.sh
+++ Disable-Password-Auth.sh
@@ -1,12 +1,12 @@
-if [[ $(grep 'Include \/etc\/ssh\/sshd_config.d\/\*\.conf' /etc/ssh/sshd_config) == '' ]]; then
-    echo "Include /etc/ssh/sshd_config.d/*.conf" >> /etc/ssh/sshd_config
+if [[ $(grep 'Include \/etc\/ssh\/sshd_config\.d\/\*\.conf' /etc/ssh/sshd_config) == '' ]]; then
+    sed -i '1s/^/Include \/etc\/ssh\/sshd_config\.d\/\*\.conf\n/g' /etc/ssh/sshd_config
 else
-    if [[ $(grep '^#.*Include \/etc\/ssh\/sshd_config.d\/\*\.conf$' /etc/ssh/sshd_config) != '' ]]; then
-        sed -i 's/^#.*Include \/etc\/ssh\/sshd_config.d\/\*\.conf$/Include \/etc\/ssh\/sshd_config.d\/\*\.conf/g' /etc/ssh/sshd_config
+    if [[ $(grep '^#.*Include \/etc\/ssh\/sshd_config\.d\/\*\.conf.*$' /etc/ssh/sshd_config) != '' ]]; then
+        sed -i 's/^#.*Include \/etc\/ssh\/sshd_config\.d\/\*\.conf.*$/Include \/etc\/ssh\/sshd_config\.d\/\*\.conf/g' /etc/ssh/sshd_config
     fi
 fi
 
-if [[ $(ls /etc/ssh | grep 'sshd_config.d') == '' ]]; then
+if [[ $(ls /etc/ssh | grep 'sshd_config\.d') == '' ]]; then
     mkdir /etc/ssh/sshd_config.d
 fi
 
@@ -16,8 +16,8 @@ sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_
 if [[ $(ls /etc/ssh/sshd_config.d | grep 'disable-password-auth\.conf') == '' ]]; then
     echo "PasswordAuthentication no" >> /etc/ssh/sshd_config.d/disable-password-auth.conf
 else
-    if [[ $(grep '^#.*PasswordAuthentication no$' /etc/ssh/sshd_config.d/disable-password-auth.conf) != '' ]]; then
-        sed -i 's/^#.*PasswordAuthentication no$/PasswordAuthentication no/g' /etc/ssh/sshd_config.d/disable-password-auth.conf
+    if [[ $(grep '^#.*PasswordAuthentication no.*$' /etc/ssh/sshd_config.d/disable-password-auth.conf) != '' ]]; then
+        sed -i 's/^#.*PasswordAuthentication no.*$/PasswordAuthentication no/g' /etc/ssh/sshd_config.d/disable-password-auth.conf
     fi
 fi
 
