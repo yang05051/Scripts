58de8625f2390f7e589e3c49a0690fa8ab6a57c1
Daniel <31024419+yang05051@users.noreply.github.com>
2023-12-03 01:02:03 -0800

    Update Permit-Root-Login.sh

--- Permit-Root-Login.sh
+++ Permit-Root-Login.sh
@@ -1,5 +1,5 @@
 if [[ $(grep 'Include \/etc\/ssh\/sshd_config\.d\/\*\.conf' /etc/ssh/sshd_config) == '' ]]; then
-    sed -i '1s/^/Include \/etc\/ssh\/sshd_config\.d\/\*\.conf/g' /etc/ssh/sshd_config
+    sed -i '1s/^/Include \/etc\/ssh\/sshd_config\.d\/\*\.conf\n/g' /etc/ssh/sshd_config
 else
     if [[ $(grep '^#.*Include \/etc\/ssh\/sshd_config\.d\/\*\.conf$' /etc/ssh/sshd_config) != '' ]]; then
         sed -i 's/^#.*Include \/etc\/ssh\/sshd_config\.d\/\*\.conf$/Include \/etc\/ssh\/sshd_config.d\/\*\.conf/g' /etc/ssh/sshd_config
