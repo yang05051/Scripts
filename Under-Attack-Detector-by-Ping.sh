95856f68e1db63b2129fbaaf6b5e545789324429
Daniel <31024419+yang05051@users.noreply.github.com>
2023-12-29 02:14:07 -0800

    Create Under-Attack-Detector-by-Ping.sh

---
+++ Under-Attack-Detector-by-Ping.sh
@@ -0,0 +1,65 @@
+read_input () {
+  echo $@ | awk -v varnum="$1" -v varin="$@" '{split(varin, READIN, " "); print READIN[varnum]}'
+}
+
+INNUM=1
+ANSPINGDEST="1.1.1.1"
+ANSPINGMAXTHSD="100"
+ANSPINGLOSSTHSD="0"
+ANSTGBOT=""
+ANSTGCHAT=""
+ANSATTACKMSG="Your server is under attack. "
+ANSNORMALMSG="The attck to your server stops. "
+
+while [$(read_input $INNUM) != ""]
+do
+  case $(read_input $INNUM) in
+
+    "-pingdest")
+      ANSPINGDEST=$(read_input $((INNUM+1)))
+      ;;
+
+    "-pingmax")
+      ANSPINGMAXTHSD=$(read_input $((INNUM+1)))
+      ;;
+
+    "-pingloss")
+      ANSPINGLOSSTHSD=$(read_input $((INNUM+1)))
+      ;;
+
+    "-tgbot")
+      ANSTGBOT=$(read_input $((INNUM+1)))
+      ;;
+
+    "-tgchat")
+      ANSTGCHAT=$(read_input $((INNUM+1)))
+      ;;
+
+    "-attackmsg")
+      ANSATTACKMSG=$(read_input $((INNUM+1)))
+      ;;
+
+    "-normalmsg")
+      ANSNORMALMSG=$(read_input $((INNUM+1)))
+      ;;
+
+    esac
+
+    INNUM=$((INNUM+1))
+    
+done
+
+if [[ ANSTGBOT == "" || ANSTGCHAT == ""]]; then
+  echo "Argument -tgbot or -tgchat not specified. "
+  # exit 1;
+fi
+
+ANSPING=$(ping -c 4 $ANSPINGDEST -q)
+ANSPINGMAX=$(echo $ANSPING | awk '{split($26, PINGNUM, "/"); print PINGNUM[3]}')
+ANSPINGLOSS=$(echo $ANSPING | awk '{split($18, LOSSNUM, "%"); print LOSSNUM[1]}')
+
+if [[ $ANSPINGMAX > $ANSPINGMAXTHSD && $ANSPINGLOSS > $ANSPINGLOSSTHSD ]]; then
+  echo "Under Attack"
+else
+  echo "Not under attack"
+fi
