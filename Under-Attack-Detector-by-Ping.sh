b1e77fc3588b399287045695fadf6d58d1308be9
Daniel <31024419+yang05051@users.noreply.github.com>
2023-12-29 05:04:19 -0800

    Update Under-Attack-Detector-by-Ping.sh

--- Under-Attack-Detector-by-Ping.sh
+++ Under-Attack-Detector-by-Ping.sh
@@ -3,7 +3,7 @@ read_input () {
 }
 
 telegram_push () {
-  curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"chat_id\": \"$2\", \"text\": \"$3\", \"disable_notification\": false}" https://api.telegram.org/bot$1/sendMessage
+  curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"chat_id\": \"$2\", \"text\": \"$3\", \"disable_notification\": false}" https://api.telegram.org/bot$1/sendMessage;
 }
 
 INNUM="1"
