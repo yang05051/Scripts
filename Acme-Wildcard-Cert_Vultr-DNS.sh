f621f9d98ac6cb3b7d094cc820d297715cbb2235
Daniel <31024419+yang05051@users.noreply.github.com>
2024-02-08 22:43:16 +0800

    Update Acme-Wildcard-Cert_Vultr-DNS.sh

--- Acme-Wildcard-Cert_Vultr-DNS.sh
+++ Acme-Wildcard-Cert_Vultr-DNS.sh
@@ -1,9 +1,10 @@
+read -p "Enter your email: " ANSEMAIL
 read -p "Enter your domain: " ANSDOMAIN
 read -p "Enter your Vultr API key: " ANSAPIKEY
 read -p "Enter renew hook: (Leave blank to skip) " ANSRENEWHOOK
 
-if [[ $ANSDOMAIN == "" || $ANSAPIKEY == "" ]]; then
-    echo "Doamin and Vultr API key cannot be empty. "
+if [[ $ANSDOMAIN == "" || $ANSAPIKEY == "" || $ANSEMAIL == '' ]]; then
+    echo "Email, Doamin, or Vultr API key cannot be empty. "
     exit 1;
 fi
 
@@ -16,7 +17,7 @@ else
     exit 1;
 fi
 
-curl https://get.acme.sh | sh -s email=daniel@danielee.dev
+curl https://get.acme.sh | sh -s email=ANSEMAIL
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 
