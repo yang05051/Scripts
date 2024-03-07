8535eca0d644998c5b705f5431377093cf145807
Daniel <31024419+yang05051@users.noreply.github.com>
2024-03-07 23:27:00 +0800

    Update Acme-Wildcard-Cert_CF-DNS_Restricted-API-Token.sh

--- Acme-Wildcard-Cert_CF-DNS_Restricted-API-Token.sh
+++ Acme-Wildcard-Cert_CF-DNS_Restricted-API-Token.sh
@@ -29,11 +29,25 @@ esac
 read -p "Enter API token: " ANSAPITOKEN
 read -p "Enter your domain: " ANSDOMAIN
 read -p "Enter your email for registering ACME account: " ANSEMAIL
-read -p "Enter renew hook: (Leave blank to skip) " ANSRENEWHOOK
+read -p "Enter renew hook (Leave blank to skip): " ANSRENEWHOOK
+read -p "Enter key file path (Default is /root/xray.key, leave blank to use default): " ANSKEYPATH
+read -p "Enter full chain file path (Default is /root/xray.pem, leave blank to use default): " ANSFULLCHAINPATH
 
 if [[ $ANSDOMAIN == "" || $ANSAPITOKEN == "" || $ANSEMAIL == "" ]]; then
     echo "Email, Doamin, or Vultr API key cannot be empty. "
     exit 1;
+fi
+
+if [[ $ANSKEYPATH != "" ]]; then
+    KEYPATH=$ANSKEYPATH
+else
+    KEYPATH="/root/xray.key"
+fi
+
+if [[ $ANSFULLCHAINPATH != "" ]]; then
+    FULLCHAINPATH=$ANSFULLCHAINPATH
+else    
+    FULLCHAINPATH="/root/xray.pem"
 fi
 
 if [[ $(which yum) != '' ]]; then
@@ -52,7 +66,7 @@ curl https://get.acme.sh | sh -s email="$ANSEMAIL"
 export CF_Token="$ANSAPITOKEN"
 
 if [[ $ANSRENEWHOOK != "" ]]; then
-    /root/.acme.sh/acme.sh --issue --dns dns_cf -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file /root/xray.key --fullchain-file /root/xray.pem --renew-hook "$ANSRENEWHOOK"
+    /root/.acme.sh/acme.sh --issue --dns dns_cf -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file $KEYPATH --fullchain-file $FULLCHAINPATH --renew-hook "$ANSRENEWHOOK"
 else
-    /root/.acme.sh/acme.sh --issue --dns dns_cf -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file /root/xray.key --fullchain-file /root/xray.pem
+    /root/.acme.sh/acme.sh --issue --dns dns_cf -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file $KEYPATH --fullchain-file $FULLCHAINPATH
 fi
