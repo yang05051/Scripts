07cc35a8124e952bd6ca90be2c374217329f0b4e
Daniel <31024419+yang05051@users.noreply.github.com>
2024-05-26 16:44:21 +0800

    Update Acme-Wildcard-Cert_Vultr-DNS.sh

--- Acme-Wildcard-Cert_Vultr-DNS.sh
+++ Acme-Wildcard-Cert_Vultr-DNS.sh
@@ -2,10 +2,24 @@ read -p "Enter your domain: " ANSDOMAIN
 read -p "Enter your Vultr API key: " ANSAPIKEY
 read -p "Enter your email for registering ACME account: " ANSEMAIL
 read -p "Enter renew hook: (Leave blank to skip) " ANSRENEWHOOK
+read -p "Enter key file path (Default is /root/xray.key, leave blank to use default): " ANSKEYPATH
+read -p "Enter full chain file path (Default is /root/xray.pem, leave blank to use default): " ANSFULLCHAINPATH
 
 if [[ $ANSDOMAIN == "" || $ANSAPIKEY == "" || $ANSEMAIL == '' ]]; then
     echo "Doamin, Vultr API key, or Email for ACME registration cannot be empty. "
     exit 1;
+fi
+
+if [[ $ANSKEYPATH != "" ]]; then
+    KEYPATH=$ANSKEYPATH
+else
+    KEYPATH="/root/xray.key"
+fi
+
+if [[ $ANSFULLCHAINPATH != "" ]]; then
+    FULLCHAINPATH=$ANSFULLCHAINPATH
+else    
+    FULLCHAINPATH="/root/xray.pem"
 fi
 
 if [[ $(which yum) != '' ]]; then
@@ -24,7 +38,7 @@ curl https://get.acme.sh | sh -s email="$ANSEMAIL"
 export VULTR_API_KEY="$ANSAPIKEY"
 
 if [[ $ANSRENEWHOOK != "" ]]; then
-    /root/.acme.sh/acme.sh --issue --dns dns_vultr -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file /root/xray.key --fullchain-file /root/xray.pem --renew-hook "$ANSRENEWHOOK"
+    /root/.acme.sh/acme.sh --issue --dns dns_vultr -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file $KEYPATH --fullchain-file $FULLCHAINPATH --renew-hook "$ANSRENEWHOOK"
 else
-    /root/.acme.sh/acme.sh --issue --dns dns_vultr -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file /root/xray.key --fullchain-file /root/xray.pem
+    /root/.acme.sh/acme.sh --issue --dns dns_vultr -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file $KEYPATH --fullchain-file $FULLCHAINPATH
 fi
