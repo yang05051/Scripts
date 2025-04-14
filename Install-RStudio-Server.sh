a3ced279dd90411bbc0bed6e7d589ccd91622722
Daniel <31024419+yang05051@users.noreply.github.com>
2025-04-14 23:58:25 +0800

    Create Install-RStudio-Server.sh

---
+++ Install-RStudio-Server.sh
@@ -0,0 +1,18 @@
+if [[ $(which yum) != '' ]]; then
+    yum install jq -y
+elif [[ $(which apt) != '' ]]; then
+    apt install jq -y
+else
+    echo "Unsupported system."
+    exit 1;
+fi
+
+VER=$(curl -s "https://api.github.com/repos/rstudio/rstudio/tags" | jq -r '.[0].name')
+VER=${VER//+/-}
+VER=${VER//v/}
+FILENAME=https://download2.rstudio.org/server/jammy/amd64/rstudio-server-${VER}-amd64.deb
+
+wget ${FILENAME}
+apt install gdebi-core
+gdebi ${FILENAME}
+rm ${FILENAME}
