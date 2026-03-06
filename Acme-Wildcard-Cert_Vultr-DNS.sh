43ffa1ed54b6de3961a5e6ee06b2fb29b1c453a4
Daniel <31024419+yang05051@users.noreply.github.com>
2026-03-06 11:45:43 +0800

    Change key type from ec-256 to ec-384 for cert issuance

--- Acme-Wildcard-Cert_Vultr-DNS.sh
+++ Acme-Wildcard-Cert_Vultr-DNS.sh
@@ -38,7 +38,7 @@ curl https://get.acme.sh | sh -s email="$ANSEMAIL"
 export VULTR_API_KEY="$ANSAPIKEY"
 
 if [[ $ANSRENEWHOOK != "" ]]; then
-    /root/.acme.sh/acme.sh --issue --dns dns_vultr -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file $KEYPATH --fullchain-file $FULLCHAINPATH --renew-hook "$ANSRENEWHOOK"
+    /root/.acme.sh/acme.sh --issue --dns dns_vultr -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-384 --key-file $KEYPATH --fullchain-file $FULLCHAINPATH --renew-hook "$ANSRENEWHOOK"
 else
-    /root/.acme.sh/acme.sh --issue --dns dns_vultr -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-256 --key-file $KEYPATH --fullchain-file $FULLCHAINPATH
+    /root/.acme.sh/acme.sh --issue --dns dns_vultr -d $ANSDOMAIN -d *.$ANSDOMAIN -k ec-384 --key-file $KEYPATH --fullchain-file $FULLCHAINPATH
 fi
